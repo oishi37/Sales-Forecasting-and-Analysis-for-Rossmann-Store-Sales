df1389f445b67b042d756a7c90559fe51babd790
Fariha Tasnim Oishi <fariha.oishi@mail.utoronto.ca>
2024-08-23 15:15:37 -0400

    final commit

--- README.md
+++ README.md
@@ -1,10 +1,81 @@
 # Sales-Forecasting-and-Analysis-for-Rossmann-Store-Sales
+
+Follow these steps to set up and run the project:
+
+## 1. Clone the Repository
+
+```bash
+git clone https://github.com/oishi37/Sales-Forecasting-and-Analysis-for-Rossmann-Store-Sales.git
+cd server
+```
+
+## 2. Download the csv files
+
+From Kaggle https://www.kaggle.com/c/rossmann-store-sales/data download the datasets train.csv and store.csv and place both the files in the server folder.
+
+## 3. Set Up the Virtual Environment
+
+### For macOS/Linux:
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+### For Windows:
+
+```bash
+python -m venv venv
+venv\Scripts\activate
+```
+
+## 4. Install Dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+## 5. Train the Model
+
+```bash
+python3 train.py
+```
+
+### Note: Close any windows of graphs and charts that pop up to continue.
+
+## 6. Run the Server
+
+```bash
+python3 app.py
+```
+
+## 7. Run the Client
+
+```bash
+cd ..
+cd client
+```
+
+### For macOS/Linux:
+
+```bash
+open index.html
+```
+
+### For Windows:
+
+```bash
+start index.html
+```
+
 ## Introduction
+
 This project focuses on analyzing and forecasting sales data for Rossmann stores using datasets provided by Kaggle. The objective is to build predictive models to forecast future sales and to gain insights into sales patterns across various stores. The analysis involves several key steps, including data preprocessing, feature engineering, the application of time series forecasting techniques and predictive model building. The project is implemented in a Jupyter Notebook and focuses on evaluating the performance of different regression models.
 
 ## Key Features
+
 - **Time Series Analysis**: Identified cyclical patterns in the sales data using spectral analysis.
-- **Machine Learning Models**: 
+- **Machine Learning Models**:
   - **Gradient Boosting Regressor**: Gradient Boosting Regressor performed the best with a MAE of 686.10 and an R² of 90.27%, making it the most accurate model for this dataset.
   - **Random Forest Regressor**: Also performed well but slightly less accurate than Gradient Boosting.
   - **ElasticNet Regression**: Showed the least accuracy, indicating the limitations of linear models for this task.
@@ -12,15 +83,39 @@ This project focuses on analyzing and forecasting sales data for Rossmann stores
 - **Model Evaluation**: Metrics such as Mean Absolute Error (MAE), Mean Squared Error (MSE), Root Mean Squared Error (RMSE), and R² score were used to assess model accuracy.
 
 ## Dataset
+
 The data used here is from Kaggle https://www.kaggle.com/c/rossmann-store-sales/data. The dataset used for this project includes:
+
 - **Sales Data**: Historical sales records.
 - **Store Data**: Information about different stores, including types and locations.
 
 ## Models Used
+
 - **Gradient Boosting Regressor**
 - **Random Forest Regressor**
 - **ElasticNet Regression**
 
+## Feature Descriptions and Input Limitations
+
+This project utilizes several features to predict sales. Below is a description of each feature and any input limitations:
+
+Day of the Week (1-7): Numerical value representing the day of the week, where 1 = Monday and 7 = Sunday.
+Number of Customers: The number of customers on a given day. This input is required for accurate predictions.
+Promo (0 or 1): Indicates whether a store is running a promotion on that day (1 = Yes, 0 = No).
+Assortment_a (0 or 1): Indicates whether the store has Assortment level 'a' (1 = Yes, 0 = No). Assortment 'a' represents a basic assortment.
+Assortment_c (0 or 1): Indicates whether the store has Assortment level 'c' (1 = Yes, 0 = No). Assortment 'c' represents an extended assortment
+Promo2 (0 or 1): Indicates whether the store is participating in Promo2, a continuous and consecutive promotion (1 = Yes, 0 = No).
+Sales_Lag1: The sales value of the previous day. This is to capture the impact of sales trends from one day to the next.
+Sales_Lag7: The sales value from the same day in the previous week. Useful for capturing weekly sales patterns.
+Month (1-12): The month of the year, represented numerically where 1 = January and 12 = December.
+Week of the Year (1-52): The week of the year, used to account for seasonality and other temporal effects.
+
+## Input Limitations:
+
+Numeric Inputs: Ensure that all numeric inputs are within the expected ranges (e.g., Days of the Week should be between 1 and 7).
+Binary Inputs: Inputs like Promo, Assortment_a, Assortment_c, and Promo2 should only be 0 or 1.
+Sales Lag Features: It is recommended to input realistic lag values based on historical sales data to maintain prediction accuracy. (Usually between 0 to 7)
 
 ## Conclusion
+
 The project demonstrates the effectiveness of ensemble methods like Gradient Boosting and Random Forest for sales forecasting, particularly when dealing with non-linear and cyclical data. The Gradient Boosting Regressor is recommended for future forecasting tasks due to its superior accuracy.
--- server/train.py
+++ server/train.py
@@ -1,4 +1,5 @@
 # Installing libraries
+from statsmodels.tsa.stattools import kpss
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -30,37 +31,41 @@ from scipy.signal import periodogram
 from scipy.stats import chi2
 import pickle
 
-
-
 # Load datasets with explicit dtypes to avoid DtypeWarning
-train_df = pd.read_csv('train.csv', dtype={'StateHoliday': 'str', 'Sales': 'float64', 'Customers': 'int64', 'Open': 'int64'}, low_memory=False)
-store_df = pd.read_csv('store.csv', dtype={'CompetitionDistance': 'float64', 'Promo2SinceYear': 'float64'}, low_memory=False)
+print("Loading data...")
+train_df = pd.read_csv('train.csv', dtype={
+                       'StateHoliday': 'str', 'Sales': 'float64', 'Customers': 'int64', 'Open': 'int64'}, low_memory=False)
+store_df = pd.read_csv('store.csv', dtype={
+                       'CompetitionDistance': 'float64', 'Promo2SinceYear': 'float64'}, low_memory=False)
 
 # Impute missing values using median or mode, with loc to avoid chained assignment warning
-
+print("Preprocessing data...")
 train_df['StateHoliday'] = train_df['StateHoliday'].astype(str)
 
-store_df.loc[:, 'CompetitionDistance'] = store_df['CompetitionDistance'].fillna(store_df['CompetitionDistance'].median())
-store_df.loc[:, 'CompetitionOpenSinceMonth'] = store_df['CompetitionOpenSinceMonth'].fillna(store_df['CompetitionOpenSinceMonth'].mode()[0])
-store_df.loc[:, 'CompetitionOpenSinceYear'] = store_df['CompetitionOpenSinceYear'].fillna(store_df['CompetitionOpenSinceYear'].mode()[0])
-store_df.loc[:, 'Promo2SinceWeek'] = store_df['Promo2SinceWeek'].fillna(store_df['Promo2SinceWeek'].mode()[0])
-store_df.loc[:, 'Promo2SinceYear'] = store_df['Promo2SinceYear'].fillna(store_df['Promo2SinceYear'].mode()[0])
+store_df.loc[:, 'CompetitionDistance'] = store_df['CompetitionDistance'].fillna(
+    store_df['CompetitionDistance'].median())
+store_df.loc[:, 'CompetitionOpenSinceMonth'] = store_df['CompetitionOpenSinceMonth'].fillna(
+    store_df['CompetitionOpenSinceMonth'].mode()[0])
+store_df.loc[:, 'CompetitionOpenSinceYear'] = store_df['CompetitionOpenSinceYear'].fillna(
+    store_df['CompetitionOpenSinceYear'].mode()[0])
+store_df.loc[:, 'Promo2SinceWeek'] = store_df['Promo2SinceWeek'].fillna(
+    store_df['Promo2SinceWeek'].mode()[0])
+store_df.loc[:, 'Promo2SinceYear'] = store_df['Promo2SinceYear'].fillna(
+    store_df['Promo2SinceYear'].mode()[0])
 
-# convert Date columns to datetime 
+# convert Date columns to datetime
 train_df['Date'] = pd.to_datetime(train_df['Date'])
 
 
-
-
-# check the datasets again 
+# check the datasets again
 # print(train_df.info())
 # print(test_df.info())
 # print(store_df.info())
 
 
-
 # List of months to create columns for
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
 
 # Initialize new columns for each month with 0
 for month in months:
@@ -89,6 +94,7 @@ merged_df = merged_df[merged_df['Open'] == 1]
 
 ###############
 # Extract Date Features like Year, Month, Day, WeekOfYear to help in capturing seasonality and trends.
+print("Extracting features...")
 merged_df['Year'] = merged_df['Date'].dt.year
 merged_df['Month'] = merged_df['Date'].dt.month
 merged_df['Day'] = merged_df['Date'].dt.day
@@ -107,35 +113,40 @@ merged_df = pd.get_dummies(merged_df, columns=['StateHoliday'])
 
 
 # Scaling ensures that all features contribute equally to the analysis: normalization and standardization
+print("Normalizing features...")
 # Initialize the scaler
 scaler = StandardScaler()
 
-# List of columns to scale 
-columns_to_scale = ['Sales_Lag1', 'Sales_Lag7', 'CompetitionDistance', 'Customers']
+# List of columns to scale
+columns_to_scale = ['Sales_Lag1', 'Sales_Lag7',
+                    'CompetitionDistance', 'Customers']
 
 # Apply scaling
 merged_df[columns_to_scale] = scaler.fit_transform(merged_df[columns_to_scale])
 
-## STEP 3
-# time series decomposition 
+# STEP 3
+# time series decomposition
 daily_sales = merged_df.groupby('Date')['Sales'].sum().reset_index()
 
-decomposition = seasonal_decompose(daily_sales['Sales'], model='multiplicative', period=30) # period can be adjusted
+decomposition = seasonal_decompose(
+    # period can be adjusted
+    daily_sales['Sales'], model='multiplicative', period=30)
 trend = decomposition.trend
 seasonal = decomposition.seasonal
 residual = decomposition.resid
 decomposition.plot()
 plt.show()
 
-# trend detection 
-daily_sales['Sales_MA'] = daily_sales['Sales'].rolling(window=30).mean()  # 30-day moving average
-daily_sales[['Date', 'Sales', 'Sales_MA']].plot(x='Date', figsize=(14,7))
+# trend detection
+daily_sales['Sales_MA'] = daily_sales['Sales'].rolling(
+    window=30).mean()  # 30-day moving average
+daily_sales[['Date', 'Sales', 'Sales_MA']].plot(x='Date', figsize=(14, 7))
 plt.show()
 
 
-
 # Prepare the data
-daily_sales['Date_ordinal'] = pd.to_datetime(daily_sales['Date']).map(lambda date: date.toordinal())
+daily_sales['Date_ordinal'] = pd.to_datetime(
+    daily_sales['Date']).map(lambda date: date.toordinal())
 X = daily_sales['Date_ordinal'].values.reshape(-1, 1)
 y = daily_sales['Sales'].values
 
@@ -144,19 +155,19 @@ model = LinearRegression()
 model.fit(X, y)
 trend = model.predict(X)
 
-#the original data and the trend line
-plt.figure(figsize=(10,6))
+# the original data and the trend line
+plt.figure(figsize=(10, 6))
 plt.plot(daily_sales['Date'], daily_sales['Sales'], label='Original Sales')
-plt.plot(daily_sales['Date'], trend, color='red', label='Linear Trend', linewidth=2)
+plt.plot(daily_sales['Date'], trend, color='red',
+         label='Linear Trend', linewidth=2)
 plt.legend()
 plt.title('Sales with Linear Trend')
 plt.show()
 
-# Seasonlity detection 
+# Seasonlity detection
 monthly_sales = merged_df.groupby(merged_df['Date'].dt.month)['Sales'].sum()
-monthly_sales.plot(kind='bar', figsize=(10,5))
+monthly_sales.plot(kind='bar', figsize=(10, 5))
 plt.show()
-
 
 
 # Fourier transform to detect cycles within the data.
@@ -167,20 +178,19 @@ plt.show()
 
 # SARIMA to Predict future sales considering both trend and seasonality.
 
-# test for stationary 
+# test for stationary
 # ADF Test
 adf_result = adfuller(daily_sales['Sales'])
 print('ADF Statistic:', adf_result[0])
 print('p-value:', adf_result[1])
 
 # KPSS Test
-from statsmodels.tsa.stattools import kpss
 kpss_result = kpss(daily_sales['Sales'], regression='c')
 print('KPSS Statistic:', kpss_result[0])
 print('p-value:', kpss_result[1])
 
 # ACF and PACF plots
-fig, ax = plt.subplots(2, figsize=(12,8))
+fig, ax = plt.subplots(2, figsize=(12, 8))
 
 plot_acf(daily_sales['Sales'].dropna(), ax=ax[0])
 plot_pacf(daily_sales['Sales'].dropna(), ax=ax[1])
@@ -188,8 +198,8 @@ plot_pacf(daily_sales['Sales'].dropna(), ax=ax[1])
 plt.show()
 
 # the SARIMA model
-model = SARIMAX(daily_sales['Sales'], 
-                order=(1, 0, 1), 
+model = SARIMAX(daily_sales['Sales'],
+                order=(1, 0, 1),
                 seasonal_order=(1, 0, 1, 12),
                 enforce_stationarity=False,
                 enforce_invertibility=False)
@@ -198,48 +208,50 @@ model = SARIMAX(daily_sales['Sales'],
 
 results = model.fit()
 print(results.summary())
-daily_sales['forecast'] = results.predict(start=0, end=len(daily_sales)-1, dynamic=False)
-daily_sales[['Sales', 'forecast']].plot(figsize=(10,5))
+daily_sales['forecast'] = results.predict(
+    start=0, end=len(daily_sales)-1, dynamic=False)
+daily_sales[['Sales', 'forecast']].plot(figsize=(10, 5))
 plt.show()
 
-### check the residue since forcast closelsy follows the data, so like overfitting
+# check the residue since forcast closelsy follows the data, so like overfitting
 # Extracting the residuals from the fitted model
 residuals = results.resid
 
 # Plot residuals
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(10, 6))
 sns.histplot(residuals, kde=True)
 plt.title('Histogram of Residuals')
 plt.show()
 
 # QQ-Plot
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(10, 6))
 stats.probplot(residuals, dist="norm", plot=plt)
 plt.title('QQ Plot of Residuals')
 plt.show()
 
 # Box-Cox transformation
-daily_sales['BoxCox_Sales'], fitted_lambda = boxcox(daily_sales['Sales'] + 1)  # Adding 1 to handle zeros
+daily_sales['BoxCox_Sales'], fitted_lambda = boxcox(
+    daily_sales['Sales'] + 1)  # Adding 1 to handle zeros
 
 # Fit SARIMA model on Box-Cox transformed data
-model = SARIMAX(daily_sales['BoxCox_Sales'], 
-                order=(1, 0, 1), 
+model = SARIMAX(daily_sales['BoxCox_Sales'],
+                order=(1, 0, 1),
                 seasonal_order=(1, 0, 1, 12),
                 enforce_stationarity=False,
                 enforce_invertibility=False)
 results = model.fit()
 
-#the residuals
+# the residuals
 residuals = results.resid
 
 # histogram of residuals
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(10, 6))
 sns.histplot(residuals, kde=True)
 plt.title('Histogram of Residuals (After Box-Cox Transformation)')
 plt.show()
 
 # QQ-Plot
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(10, 6))
 stats.probplot(residuals, dist="norm", plot=plt)
 plt.title('QQ Plot of Residuals (After Box-Cox Transformation)')
 plt.show()
@@ -247,7 +259,7 @@ plt.show()
 #  the fitted lambda
 print("Fitted Lambda for Box-Cox Transformation:", fitted_lambda)
 
-## normality is better but not fixed. 
+# normality is better but not fixed.
 
 # After the box-cox spectral Analysis
 # Extract residuals from SARIMA model
@@ -277,8 +289,10 @@ print(f"95% Confidence Interval: [{lower_bound}, {upper_bound}]")
 # the periodogram with confidence intervals
 plt.figure(figsize=(10, 6))
 plt.plot(frequencies, spectral_density, label='Spectral Density')
-plt.axvline(predominant_frequency, color='r', linestyle='--', label='Predominant Frequency')
-plt.fill_between(frequencies, lower_bound, upper_bound, color='gray', alpha=0.3, label='95% Confidence Interval')
+plt.axvline(predominant_frequency, color='r',
+            linestyle='--', label='Predominant Frequency')
+plt.fill_between(frequencies, lower_bound, upper_bound,
+                 color='gray', alpha=0.3, label='95% Confidence Interval')
 plt.xlabel('Frequency')
 plt.ylabel('Spectral Density')
 plt.title('Periodogram of Residuals')
@@ -293,13 +307,15 @@ merged_df['StoreType'] = label_encoder.fit_transform(merged_df['StoreType'])
 
 # Proceed with your analysis or modeling
 # Example: Aggregating total sales by store
-store_sales = merged_df.groupby(['Store', 'StoreType'])['Sales'].sum().reset_index()
+store_sales = merged_df.groupby(['Store', 'StoreType'])[
+    'Sales'].sum().reset_index()
 
 # Aggregate average sales by store type
-store_type_sales = store_sales.groupby('StoreType')['Sales'].mean().reset_index()
+store_type_sales = store_sales.groupby(
+    'StoreType')['Sales'].mean().reset_index()
 
 # Bar Plot for Average Sales by Store Type:
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(10, 6))
 sns.barplot(x='StoreType', y='Sales', data=store_type_sales)
 plt.title('Average Sales by Store Type')
 plt.xlabel('Store Type')
@@ -307,21 +323,21 @@ plt.ylabel('Average Sales')
 plt.show()
 
 # Box Plot to Show Distribution of Sales within Each Store Type:
-plt.figure(figsize=(12,8))
+plt.figure(figsize=(12, 8))
 sns.boxplot(x='StoreType', y='Sales', data=store_sales)
 plt.title('Sales Distribution by Store Type')
 plt.xlabel('Store Type')
 plt.ylabel('Total Sales per Store')
 plt.show()
-#### 
-
+####
 
 
 ####
 # Top 5 Stores by Sales from Each Store Type
-top_stores = store_sales.groupby('StoreType').apply(lambda x: x.nlargest(5, 'Sales')).reset_index(drop=True)
+top_stores = store_sales.groupby('StoreType').apply(
+    lambda x: x.nlargest(5, 'Sales')).reset_index(drop=True)
 
-plt.figure(figsize=(14,8))
+plt.figure(figsize=(14, 8))
 sns.barplot(x='Store', y='Sales', hue='StoreType', data=top_stores)
 plt.title('Top 5 Stores by Sales for Each Store Type')
 plt.xlabel('Store')
@@ -333,11 +349,12 @@ plt.show()
 # a sample of 50 stores to avoid overcrowding
 sampled_stores = store_sales.sample(50, random_state=42)
 
-#data for the heatmap
-sales_pivot = sampled_stores.pivot(index="Store", columns="StoreType", values="Sales")
+# data for the heatmap
+sales_pivot = sampled_stores.pivot(
+    index="Store", columns="StoreType", values="Sales")
 
-#heatmap
-plt.figure(figsize=(14,10))
+# heatmap
+plt.figure(figsize=(14, 10))
 sns.heatmap(sales_pivot, annot=False, cmap="YlGnBu")
 plt.title('Sales Heatmap for a Sample of 50 Stores')
 plt.xlabel('Store Type')
@@ -350,9 +367,9 @@ merged_df = merged_df.fillna(0)
 # List of columns to include in X
 feature_columns = [
     'Store', 'DayOfWeek', 'Customers', 'Open', 'Promo', 'StateHoliday_a',
-    'StateHoliday_b', 'StateHoliday_c', 'SchoolHoliday', 'StoreType', 'Assortment_a', 
-    'Assortment_b', 'Assortment_c', 'CompetitionDistance', 'Promo2', 
-    'Promo2SinceWeek', 'Promo2SinceYear', 'Sales_Lag1', 'Sales_Lag7', 
+    'StateHoliday_b', 'StateHoliday_c', 'SchoolHoliday', 'StoreType', 'Assortment_a',
+    'Assortment_b', 'Assortment_c', 'CompetitionDistance', 'Promo2',
+    'Promo2SinceWeek', 'Promo2SinceYear', 'Sales_Lag1', 'Sales_Lag7',
     'Month', 'Year', 'WeekOfYear'
 ]
 
@@ -380,10 +397,10 @@ y_test_list = []
 # Looping through each store
 for store_id in merged_df['Store'].unique():
     store_data = merged_df[merged_df['Store'] == store_id]
-    
+
     # putting the index for splitting based on time for this store
     split_index = int(len(store_data) * 0.8)
-    
+
     # then split the data for this store
     X_train_list.append(store_data.iloc[:split_index].drop('Sales', axis=1))
     X_test_list.append(store_data.iloc[split_index:].drop('Sales', axis=1))
@@ -404,12 +421,14 @@ X_test = X_test.drop(columns=['Store'])
 X_train = X_train[selected_features]
 X_test = X_test[selected_features]
 
-#missing values in lag features in X_train
+# missing values in lag features in X_train
 X_train['Sales_Lag1'].fillna(0, inplace=True)
 X_train['Sales_Lag7'].fillna(0, inplace=True)
 
+print("Training Random Forest Regressor...")
 # Random Forest Regressor
-rf_model = RandomForestRegressor(n_estimators=100, max_depth=5, random_state=42)
+rf_model = RandomForestRegressor(
+    n_estimators=100, max_depth=5, random_state=42)
 rf_model.fit(X_train, y_train)
 
 # predictions on the test set
@@ -430,7 +449,9 @@ X_test['Assortment_c'] = X_test['Assortment_c'].astype(int)
 
 
 # Gradient Boosting Regressor
-gb_model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=5, random_state=42)
+print("Training Gradient Boosting Regressor...")
+gb_model = GradientBoostingRegressor(
+    n_estimators=100, learning_rate=0.1, max_depth=5, random_state=42)
 gb_model.fit(X_train, y_train)
 
 # Predictions on the test set
@@ -443,6 +464,7 @@ rmse_gb = np.sqrt(mse_gb)
 r2_gb = r2_score(y_test, y_pred_gb) * 100  # R² score as a percentage
 
 # Elastic Net Regressor
+print("Training Elastic Net Regressor...")
 en_model = ElasticNet(alpha=1.0, l1_ratio=0.5, random_state=42)
 en_model.fit(X_train, y_train)
 
@@ -456,13 +478,14 @@ rmse_en = np.sqrt(mse_en)
 r2_en = r2_score(y_test, y_pred_en) * 100  # R² score as a percentage
 
 # Re-train the Gradient Boosting Regressor on the entire dataset
-gbr_model = GradientBoostingRegressor(n_estimators=100, max_depth=5, random_state=42)
+gbr_model = GradientBoostingRegressor(
+    n_estimators=100, max_depth=5, random_state=42)
 gbr_model.fit(X_reduced, y)
-              
+
 future_sales_predictions = gbr_model.predict(X_reduced)
 
 # Plotting the predicted future sales
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(10, 6))
 plt.plot(future_sales_predictions, label='Predicted Sales')
 plt.title('Future Sales Predictions')
 plt.xlabel('Time (e.g., days, weeks)')
@@ -471,5 +494,8 @@ plt.legend()
 plt.show()
 
 # Save the model
+print("Saving trained model...")
 with open('model.pkl', 'wb') as file:
     pickle.dump(gbr_model, file)
+
+print("Saved.")
